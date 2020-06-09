1da74d1f09c2b5ba9c0448d31bb047219761ced8
murugesan45 <murugesand45@gmail.com>
2020-06-09 13:50:58 +0530

    Changes in Encryption And Decryption

--- src/common/EncryptionAndDecryption.ts
+++ src/common/EncryptionAndDecryption.ts
@@ -6,20 +6,18 @@ export class EncryptionAndDecryption
 {
  algorithm = 'aes-256-cbc';
  key = crypto.randomBytes(32);
- iv = crypto.randomBytes(16);
-
-
+ iv = "TheEncryptionKey";
+ 
  encrypt(text) {
  let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(this.key), this.iv);
  let encrypted = cipher.update(text);
  encrypted = Buffer.concat([encrypted, cipher.final()]);
- return { iv: this.iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+ return encrypted.toString('hex');
 }
 
  decrypt(text) {
- let iv = Buffer.from(text.iv, 'hex');
- let encryptedText = Buffer.from(text.encryptedData, 'hex');
- let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(this.key), iv);
+ let encryptedText = Buffer.from(text, 'hex');
+ let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(this.key), this.iv);
  let decrypted = decipher.update(encryptedText);
  decrypted = Buffer.concat([decrypted, decipher.final()]);
  return decrypted.toString();
